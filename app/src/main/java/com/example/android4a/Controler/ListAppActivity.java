3e14f10c8a9e9fcba4b2a0225484d24c3a0755d5
NelNawe <hornela.nawessi@yahoo.com>
2019-12-13 21:08:39 +0100

    init color

--- app/src/main/java/com/example/android4a/Controler/ListAppActivity.java
+++ app/src/main/java/com/example/android4a/Controler/ListAppActivity.java
@@ -4,6 +4,11 @@ package com.example.android4a.Controler;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.android4a.Model.CoiffureItem;
+
 public class ListAppActivity extends AppCompatActivity {
 
+    public static CoiffureItem get(int position) {
+        return null;
+    }
 }
--- app/src/main/java/com/example/android4a/Model/CoiffureItemAdapter.java
+++ app/src/main/java/com/example/android4a/Model/CoiffureItemAdapter.java
@@ -1,4 +1,23 @@
 package com.example.android4a.Model;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.android4a.Controler.ListAppActivity;
+import com.example.android4a.R;
+
+import java.util.List;
+
 public class CoiffureItemAdapter {
+   
+
+
 }
